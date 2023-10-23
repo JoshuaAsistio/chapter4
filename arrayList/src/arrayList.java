dcc41476d5e043777415ad175cb1fb5bbdd4a880
Elijah Joshua Asistio <salazarelijahjoshuaasistio@gmail.com>
2023-10-22 20:58:59 -1200

    Commit array list folder

---
+++ arrayList/src/arrayList.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+
+public class arrayList {    
+    public static void main(String[] args) {    	
+    	// TODO, add your application code
+    	// ArrayList<String> names = new ArrayList<String>();
+
+        // for (int i = 0; i < 6; i++) {
+        //     String name = JOptionPane.showInputDialog(null, "Add a name");
+
+        //     names.add(i, name);
+        // }
+
+        // JOptionPane.showMessageDialog(null, names);
+    }
+}
